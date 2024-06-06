7a90f32339203967715fb1641a141512f32b4eeb
Грицина Максим <bob217@mail.ru>
2024-06-06 23:06:30 +0700

    Наброски игрального стола
    - Сделали игровой стол немного привлекательнее (разукрасили карты, нарисовали колоду)
    - Сделали выделение карты для дальнейшего использования
    - Сделали набросок для метода атаки

--- PlayingCards.Durak.Web/Controllers/HomeController.cs
+++ PlayingCards.Durak.Web/Controllers/HomeController.cs
@@ -102,6 +102,22 @@ namespace PlayingCards.Durak.Web.Controllers
             }
         }
 
+        [HttpPost]
+        public void Attack(AttackModel model)
+        {
+            Table? playerTable = null;
+            Player? player = null;
+            foreach (var table in _tables.Values)
+            {
+                if (table.PlayerSecrets.ContainsKey(model.PlayerSecret))
+                {
+                    playerTable = table;
+                    player = table.PlayerSecrets[model.PlayerSecret];
+                }
+            }
+            player.Hand.Attack(model.CardIndex);
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ PlayingCards.Durak.Web/Models/AttackModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PlayingCards.Durak.Web.Models
+{
+    public class AttackModel
+    {
+        public string PlayerSecret { get; set; }
+
+        public string PlayerName { get; set; }
+
+        public Guid TableId { get; set; }
+
+        public int CardIndex { get; set; }
+    }
+}
